da84eda2d093d3f8358106799dd137481a002b0b
stygma <73248181+stygmaskyzoo@users.noreply.github.com>
2022-05-18 15:28:36 +0200

    randomizer support

---
+++ Plugins/Randomizer X/Config.rb
@@ -0,0 +1,15 @@
+#===============================================================================
+#  Randomizer Functionality for vanilla Essentials
+#-------------------------------------------------------------------------------
+#  System settings
+#===============================================================================
+module Randomizer
+  # list of trainers to exclude from the randomizer
+  EXCLUSIONS_TRAINERS = []
+
+  # list of species to exclude from the randomizer
+  EXCLUSIONS_SPECIES = []
+
+  # list of items to exclude from the randomizer
+  EXCLUSIONS_ITEMS = [:HM01, :HM02, :HM03, :HM04, :HM05, :HM06]
+end
