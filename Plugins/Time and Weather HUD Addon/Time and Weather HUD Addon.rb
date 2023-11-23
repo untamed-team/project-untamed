1cf5fa3f4fc993c831833b5864345c4b62cd2660
locomotivo <unleft@naver.com>
2023-11-23 23:20:09 +0900

    Weather HUD & Following Pokemon EX fix

--- Plugins/Time and Weather HUD Addon/Time and Weather HUD Addon.rb
+++ Plugins/Time and Weather HUD Addon/Time and Weather HUD Addon.rb
@@ -13,21 +13,6 @@ class DateAndTimeHud < Component
     @sprites["overlay"].x = Graphics.width
     @baseColor = MENU_TEXTCOLOR[$PokemonSystem.current_menu_theme] || Color.new(248,248,248)
     @shadowColor = MENU_TEXTOUTLINE[$PokemonSystem.current_menu_theme] || Color.new(48,48,48)
-	
-	#added by Gardenette to display icons based on time of day and overworld weather
-	@sprites["time_icon"] = IconSprite.new(0, 0, viewport)
-    @sprites["time_icon"].setBitmap("Graphics/Pictures/Voltseon's Pause Menu/time/day")
-	@sprites["time_icon"].visible = false
-    @sprites["time_icon"].x = Graphics.width - 124
-    @sprites["time_icon"].y = 42
-    @sprites["time_icon"].z = 99999
-	
-	@sprites["weather_icon"] = IconSprite.new(0, 0, viewport)
-    @sprites["weather_icon"].setBitmap("Graphics/Pictures/Voltseon's Pause Menu/weather/sun")
-	@sprites["weather_icon"].visible = false
-    @sprites["weather_icon"].x = @sprites["time_icon"].x - @sprites["weather_icon"].width
-    @sprites["weather_icon"].y = 42
-    @sprites["weather_icon"].z = 99999
   end
 
   def shouldDraw?; return !(pbInBugContest? || pbInSafari?); end
@@ -40,46 +25,60 @@ class DateAndTimeHud < Component
   def refresh
     text = _INTL("{1} {2} {3}",Time.now.day.to_i,pbGetAbbrevMonthName(Time.now.month.to_i),Time.now.year.to_i)
     text2 = _INTL("{1}",pbGetTimeNow.strftime("%I:%M %p"))
-    @sprites["overlay"].bitmap.clear
-    pbSetSystemFont(@sprites["overlay"].bitmap)
-    pbDrawTextPositions(@sprites["overlay"].bitmap,[[text,Graphics.width/2 - 8, 12,1,
-      @baseColor,@shadowColor],[text2,Graphics.width/2 - 8,44,1,@baseColor,@shadowColor]])
+	overlay = @sprites["overlay"].bitmap
+	overlay.clear
+    pbSetSystemFont(overlay)
+    pbDrawTextPositions(overlay,[
+		[text, Graphics.width/2 - 8, 12, 1, @baseColor, @shadowColor],
+		[text2, Graphics.width/2 - 8, 44, 1, @baseColor, @shadowColor]
+	])
     @last_time = pbGetTimeNow.strftime("%I:%M %p")
 	
+	imagepos = []
+	textwidth = overlay.text_size(text2).width
 	#added by Gardenette to display icons based on time of day and overworld weather
-	time_icon = "morning" if PBDayNight.isMorning?
-	time_icon = "day" if PBDayNight.isDay?
-	time_icon = "afternoon" if PBDayNight.isAfternoon?
-	time_icon = "evening" if PBDayNight.isEvening?
-	time_icon = "night" if PBDayNight.isNight?
+	time = -1
+	if PBDayNight.isMorning?
+	  time = 0
+	elsif PBDayNight.isDay?
+	  time = 1
+	elsif PBDayNight.isAfternoon?
+	  time = 1
+	elsif PBDayNight.isEvening?
+	  time = 2
+	elsif PBDayNight.isNight?
+	  time = 3
+	end
+
+	if time >= 0
+	  imagepos.push(["Graphics/Pictures/Voltseon's Pause Menu/weather", Graphics.width/2 - textwidth - 36, 42, 0, 24 * time, 24, 24])
+	end
 	
-	@sprites["time_icon"].setBitmap("Graphics/Pictures/Voltseon's Pause Menu/time/#{time_icon}")
-	@sprites["time_icon"].visible = true
-	
+	weather = -1
 	case $game_screen.weather_type
 	when :Rain 
-	  weather_icon = "rain"
+	  weather = 4
 	when :HeavyRain
-	  weather_icon = "rain"
+	  weather = 5
 	when :Storm
-	  weather_icon = "storm"
+	  weather = 6
 	when :Snow
-	  weather_icon = "snow"
+	  weather = 7
 	when :Blizzard
-	  weather_icon = "snow"
+	  weather = 8
 	when :Sandstorm
-	  weather_icon = "sandstorm"
+	  weather = 9
 	when :Fog
-	  weather_icon = "sandstorm"
+	  weather = 10
 	when :Sun
-	  weather_icon = "sun"
+	  weather = 1
+	end
+
+	if weather >= 0
+	  imagepos.push(["Graphics/Pictures/Voltseon's Pause Menu/weather", Graphics.width/2 - textwidth - 64, 42, 0, 24 * weather, 24, 24])
 	end
 	
-	@sprites["weather_icon"].setBitmap("Graphics/Pictures/Voltseon's Pause Menu/weather/#{weather_icon}")
-	if $game_screen.weather_type == :None
-	  @sprites["weather_icon"].visible = false
-	else
-	  @sprites["weather_icon"].visible = true
-	end
+    pbDrawImagePositions(overlay, imagepos)
+
   end
 end
