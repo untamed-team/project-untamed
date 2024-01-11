c628fae78cfa26f7d3061bcab67d0ac4eb56d799
Gardenette <106409863+Gardenette@users.noreply.github.com>
2024-01-10 19:10:48 -0500

    progress - pls don't fire me

--- Plugins/Pokeblocks/008_Cooking.rb
+++ Plugins/Pokeblocks/008_Cooking.rb
@@ -1,4 +1,34 @@
-class Cooking
+class CookingStage1
+	def initialize
+		@sprites = {}
+		@viewport = Viewport.new(0, 0, Graphics.width, Graphics.height)
+		@viewport.z = 99999
+	
+		#Graphics
+		@sprites["pot"] = IconSprite.new(0, 0, @viewport)
+		@sprites["pot"].setBitmap("Graphics/Pictures/Pokeblock/Candy Making/stewpot_base - large")
+		@sprites["pot"].x = Graphics.width/2 - @sprites["pot"].width/2
+		@sprites["pot"].y = 70
+		@sprites["pot"].z = 99999
+		
+		@sprites["stove"] = IconSprite.new(0, 0, @viewport)
+		@sprites["stove"].setBitmap("Graphics/Pictures/Pokeblock/Candy Making/stove - large")
+		@sprites["stove"].x = Graphics.width/2 - @sprites["stove"].width/2
+		@sprites["stove"].y = @sprites["pot"].y + 160
+		@sprites["stove"].z = 99998
+		
+		pbmain
+	end #def initialize
+	
+	def pbmain
+		loop do
+			Graphics.update
+			pbUpdateSpriteHash(@sprites)
+		end #loop do
+	end
+end #class CookingStage1
+
+class CookingStage2
 	def initialize
 		@sprites = {}
 		@viewport = Viewport.new(0, 0, Graphics.width, Graphics.height)
@@ -12,13 +42,26 @@ class Cooking
 		@sprites["firewood"].z = 99999
 		
 		#animname, framecount, framewidth, frameheight, frameskip
-		@sprites["fire"] = AnimatedSprite.new("Graphics/Pictures/Pokeblock/Candy Making/fire_anim",4,274,201,4,@viewport)
+		@sprites["fire"] = AnimatedSprite.new("Graphics/Pictures/Pokeblock/Candy Making/fire_anim",3,274,201,4,@viewport)
 		@sprites["fire"].x = @sprites["firewood"].x + @sprites["firewood"].width/2 - @sprites["fire"].width/4
 		@sprites["fire"].y = @sprites["firewood"].y + @sprites["firewood"].height/2 - @sprites["fire"].height/3
 		@sprites["fire"].z = 99999
 		@sprites["fire"].zoom_x = 0.5
 		@sprites["fire"].zoom_y = 0.5
 		@sprites["fire"].play
+		
+		@sprites["stove"] = IconSprite.new(0, 0, @viewport)
+		@sprites["stove"].setBitmap("Graphics/Pictures/Pokeblock/Candy Making/stove")
+		@sprites["stove"].x = Graphics.width/2 - @sprites["stove"].width/2
+		@sprites["stove"].y = @sprites["firewood"].y - @sprites["stove"].height/3
+		@sprites["stove"].z = 99999
+		
+		@sprites["pot"] = IconSprite.new(0, 0, @viewport)
+		@sprites["pot"].setBitmap("Graphics/Pictures/Pokeblock/Candy Making/stewpot_base")
+		@sprites["pot"].x = Graphics.width/2 - @sprites["pot"].width/2
+		@sprites["pot"].y = @sprites["stove"].y - 80
+		@sprites["pot"].z = 99999
+		
 		pbmain
 	end #def initialize
 	
@@ -28,4 +71,4 @@ class Cooking
 			pbUpdateSpriteHash(@sprites)
 		end #loop do
 	end
-end #class Cooking
+end #class CookingStage2
