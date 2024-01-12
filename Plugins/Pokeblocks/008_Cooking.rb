0dcddc66f36dd0e2bca6bcb3fb01e6113e455ac0
Gardenette <106409863+Gardenette@users.noreply.github.com>
2024-01-11 22:28:09 -0500

    Mouse = spoon, and spoon stir, mmmm

--- Plugins/Pokeblocks/008_Cooking.rb
+++ Plugins/Pokeblocks/008_Cooking.rb
@@ -5,24 +5,54 @@ class CookingStage1
 		@viewport.z = 99999
 	
 		#Graphics
-		@sprites["pot"] = IconSprite.new(0, 0, @viewport)
-		@sprites["pot"].setBitmap("Graphics/Pictures/Pokeblock/Candy Making/stewpot_base - large")
-		@sprites["pot"].x = Graphics.width/2 - @sprites["pot"].width/2
-		@sprites["pot"].y = 70
-		@sprites["pot"].z = 99999
+		@sprites["pot_upper"] = IconSprite.new(0, 0, @viewport)
+		@sprites["pot_upper"].setBitmap("Graphics/Pictures/Pokeblock/Candy Making/stewpot_base_upper - large")
+		@sprites["pot_upper"].x = Graphics.width/2 - @sprites["pot_upper"].width/2
+		@sprites["pot_upper"].y = 70
+		@sprites["pot_upper"].z = 99999
+		
+		@sprites["pot_lower"] = IconSprite.new(0, 0, @viewport)
+		@sprites["pot_lower"].setBitmap("Graphics/Pictures/Pokeblock/Candy Making/stewpot_base_lower - large")
+		@sprites["pot_lower"].x = Graphics.width/2 - @sprites["pot_lower"].width/2
+		@sprites["pot_lower"].y = 70
+		#always on top of other things
+		@sprites["pot_lower"].z = 999999
 		
 		@sprites["stove"] = IconSprite.new(0, 0, @viewport)
 		@sprites["stove"].setBitmap("Graphics/Pictures/Pokeblock/Candy Making/stove - large")
 		@sprites["stove"].x = Graphics.width/2 - @sprites["stove"].width/2
-		@sprites["stove"].y = @sprites["pot"].y + 160
+		@sprites["stove"].y = @sprites["pot_upper"].y + 160
 		@sprites["stove"].z = 99998
+		
+		@sprites["candy_base"] = IconSprite.new(0, 0, @viewport)
+		@sprites["candy_base"].setBitmap("Graphics/Pictures/Pokeblock/Candy Making/candy_base_in_pot")
+		@sprites["candy_base"].x = Graphics.width/2 - @sprites["candy_base"].width/2
+		@sprites["candy_base"].y = 70
+		@sprites["candy_base"].z = 99999
+		@sprites["candy_base"].visible = false
+		
+		@sprites["spoon"] = IconSprite.new(0, 0, @viewport)
+		@sprites["spoon"].setBitmap("Graphics/Pictures/Pokeblock/Candy Making/spoon")
+		@sprites["spoon"].x = Graphics.width/2 - @sprites["spoon"].width/2
+		@sprites["spoon"].y = 0
+		@sprites["spoon"].z = 99999
 		
 		pbmain
 	end #def initialize
 	
+	def updateCursorPos
+		@sprites["spoon"].x=Mouse.x-@sprites["spoon"].width/2 if defined?(Mouse.x)
+		@sprites["spoon"].y=Mouse.y-@sprites["spoon"].height+40 if defined?(Mouse.y)
+	end #updateCursorPos
+	
 	def pbmain
+		#pbMessage(_INTL("Adding candy base"))
+		@sprites["candy_base"].visible = true
+		
 		loop do
 			Graphics.update
+			Input.update
+			updateCursorPos
 			pbUpdateSpriteHash(@sprites)
 		end #loop do
 	end
