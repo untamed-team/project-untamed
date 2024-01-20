398a59633dd8ac58d5a03fde18adbfcf72f23b4a
Gardenette <106409863+Gardenette@users.noreply.github.com>
2024-01-20 09:29:05 -0500

    Deteled a test condition from quest1

--- Data/Scripts/024_Modern Quest System + UI/004_Quest Data.rb
+++ Data/Scripts/024_Modern Quest System + UI/004_Quest Data.rb
@@ -79,7 +79,6 @@ module QuestModule
     :QuestGiverSprite => "NPC DAD",
     :QuestGiverDescSprite => "DAD",
     :Stage1 => "Meet with Professor Ceiba.",
-    :TurninCondition1 => proc { $player.has_species?(:PACUNA) },
     :Stage2 => "Bring the Pokémon to Professor Ceiba's lab.",
     :Location1 => "Veravalles Town",
     :Location2 => "Professor Ceiba's Lab",
