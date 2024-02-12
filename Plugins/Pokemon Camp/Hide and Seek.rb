4413a7de588e8d0487a879d401fa6314c05d8e81
Gardenette <106409863+Gardenette@users.noreply.github.com>
2024-02-12 13:03:52 -0500

    working on hide and seek

--- Plugins/Pokemon Camp/Hide and Seek.rb
+++ Plugins/Pokemon Camp/Hide and Seek.rb
@@ -11,19 +11,43 @@ class Camping
 	
 	def assignHidingSpots
 		getCampers
+		hidingSpotsAvailable = @hidingSpots.clone
 		for i in 0...@campers.length
-			hidingSpotsAvailable = @hidingSpots.clone
+			print "We need more hiding spots on this map" if hidingSpotsAvailable.length <= 0
 			spotChosen = @hidingSpots.sample
-			@campers[i].hidingSpot = spotChosen
+			@campers[i].hideAndSeekSpot = spotChosen
+			@campers[i].hideAndSeekFound = false
 			hidingSpotsAvailable.delete(spotChosen)
-			print "#{@campers[i].name}'s hiding spot is event #{@campers[i].hidingSpot}"
+			print "#{@campers[i].name}'s hiding spot is event #{@campers[i].hideAndSeekSpot.id}"
 		end #for i in 0...@campers.length
 		#print @hidingSpots
 	end #def assignHidingSpots
 
-	def self.checkHidingSpot(spot)
+	def self.checkHidingSpot(spotChecked)
+		self.getCampers
 		#print "checking if pkmn is hiding in #{spot}"
+		
+		#check all pokemon in the party, and if they haven't been found, check if this is their hiding spot
+		for i in 0...@campers.length
+			next if @campers[i].hideAndSeekFound
+			if @campers[i].hideAndSeekFound == false
+				#is this @campers[i]'s hiding spot event?
+				print "found!" if spotChecked == @campers[i].hideAndSeekSpot
+			end #if @campers[i].hideAndSeekFound == false
+		end #for i in 0...@campers.length
+		
+		#check how many pkmn are still hiding and end hide and seek round if none left hiding
+		howManyLeft
 	end #def checkHidingSpot
+
+	def howManyLeft
+		#check all pkmn in the party and see if any are still not found
+		#for i in 0...@campers.length
+		#	if @campers[i].hideAndSeekFound == false
+		#	end
+			
+		#end #for i in 0...@campers.length
+	end #def howManyLeft
 
 	def hideAndSeek
 		findHidingSpots
@@ -31,7 +55,7 @@ class Camping
 	end #def hideAndSeek
 	
 	#on_player_interact with hiding spot
-	EventHandlers.add(:on_player_interact, :hidingSpot, proc {
+	EventHandlers.add(:on_player_interact, :hideAndSeek_CheckSpot, proc {
 		facingEvent = $game_player.pbFacingEvent
 		self.checkHidingSpot(facingEvent) if facingEvent && facingEvent.name.match(/Hiding_Spot/i)
 	})
@@ -39,5 +63,6 @@ class Camping
 end #class Camping
 
 class Pokemon
-  attr_accessor :hidingSpot
+  attr_accessor :hideAndSeekSpot
+  attr_accessor :hideAndSeekFound
 end
