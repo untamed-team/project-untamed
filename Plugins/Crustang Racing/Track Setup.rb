0f370cd6c503bf6f071d279116aa9a84a1ab09ae
Gardenette <gardenette1995@gmail.com>
2024-05-26 11:33:31 -0400

    Crustang racing - track now loops

--- Plugins/Crustang Racing/Track Setup.rb
+++ Plugins/Crustang Racing/Track Setup.rb
@@ -11,11 +11,17 @@ class CrustangRacing
 		@sprites["background"].y = 0
 		@sprites["background"].z = 99999
 		
-		@sprites["test"] = IconSprite.new(0, 0, @viewport)
-		@sprites["test"].setBitmap("Graphics/Pictures/Crustang Racing/track")
-		@sprites["test"].x = 0
-		@sprites["test"].y = 0
-		@sprites["test"].z = 99998
+		@sprites["track1"] = IconSprite.new(0, 0, @viewport)
+		@sprites["track1"].setBitmap("Graphics/Pictures/Crustang Racing/trackPart1")
+		@sprites["track1"].x = 0
+		@sprites["track1"].y = 0
+		@sprites["track1"].z = 99998
+		
+		@sprites["track2"] = IconSprite.new(0, 0, @viewport)
+		@sprites["track2"].setBitmap("Graphics/Pictures/Crustang Racing/trackPart2")
+		@sprites["track2"].x = @sprites["track1"].width
+		@sprites["track2"].y = 0
+		@sprites["track2"].z = 99998
 		
 		#set up racer hashes
 		@racer1 = {}
@@ -27,16 +33,16 @@ class CrustangRacing
 	
 	def self.drawContestants
 		racingPkmnStartingX = 20
-		racingPkmnStartingY = 64
+		racingPkmnStartingY = 52
 		i = 0
-		#3.times do
+		4.times do
 			#draw the crustang sprite with step animation on
-			filename = "Followers/FEEBAS"
+			filename = "Followers/BATHYGIGAS"
 			@sprites["racingPkmn#{i}"] = TrainerWalkingCharSprite.new(filename, @viewport)
 			charwidth  = @sprites["racingPkmn#{i}"].bitmap.width
 			charheight = @sprites["racingPkmn#{i}"].bitmap.height
-			@sprites["racingPkmn#{i}"].x        = racingPkmnStartingX - (charwidth / 8)
-			@sprites["racingPkmn#{i}"].y        = racingPkmnStartingY - (charheight / 8)
+			@sprites["racingPkmn#{i}"].x        = racingPkmnStartingX# - (charwidth / 8)
+			@sprites["racingPkmn#{i}"].y        = racingPkmnStartingY# - (charheight / 8)
 			@sprites["racingPkmn#{i}"].z = 99999
 			#sprite turn right
 			@sprites["racingPkmn#{i}"].src_rect = Rect.new(0, 128, charwidth / 4, charheight / 4)
@@ -50,14 +56,22 @@ class CrustangRacing
 			#turn up
 			#@sprites["racingPkmn#{i}"].src_rect = Rect.new(0, 128, charwidth / 4, charheight / 4)
 			
-			@racerPlayer.merge!({RacerSprite: @sprites["racingPkmn#{i}"]})
-			print @racerPlayer
+			#@racerPlayer.merge!({RacerSprite: @sprites["racingPkmn#{i}"]})
 			
-			racingPkmnStartingY += 20 + 64 #size of each follower pkmn sprite
+			racingPkmnStartingY += 8 + 64 #size of each follower pkmn sprite
 			i += 1
-		#end #3.times do
+		end #3.times do
 		
 	end #def drawContestants
+	
+	def self.trackMovementUpdate
+		@sprites["track1"].x -= 8
+		@sprites["track2"].x -= 8
+		#if track2 is now on the screen, track2's X is now 0 or less, and track1's X is still < 0, move track1 to the end of track2 for a loop
+		if @sprites["track2"].x <= 0 && @sprites["track1"].x < 0
+			@sprites["track1"].x = @sprites["track2"].x + @sprites["track2"].width - 1024
+		end
+	end #def trackMovementUpdate
 	
 	def self.main
 		self.setup
@@ -65,6 +79,7 @@ class CrustangRacing
 		loop do
 			Graphics.update
 			pbUpdateSpriteHash(@sprites)
+			self.trackMovementUpdate
 		end
 	end #def self.main
 	
