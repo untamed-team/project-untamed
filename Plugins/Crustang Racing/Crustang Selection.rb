d1c5c3e42d93a81f9f05c5718dca6eb3b424a37a
Gardenette <gardenette1995@gmail.com>
2024-07-24 23:27:15 -0400

    CR: working on crustang selection

--- Plugins/Crustang Racing/Crustang Selection.rb
+++ Plugins/Crustang Racing/Crustang Selection.rb
@@ -1,6 +1,26 @@
 class CrustangRacing
-	#selecting a crustang if you want to rent one for a race
-	def self.rentCrustang
+	def self.chooseCrustang
+		
+		############################################## if $game_variables[36] == -1 then back out
+		enteredCrustang = $game_variables[36]
+		self.main(enteredCrustang)
 	end
 	
+	#selecting a crustang if you want to rent one for a race
+	def self.rentCrustang
+		#save the player's current party
+		#replace the player's party with rentable crustang
+		self.pbChooseCrustang
+		#get the crustang that was chosen and pass it to main.rb
+		#restore user's original party
+		
+	end
+	
+	def self.chooseOwnCrustang
+		$game_variables[36] = 0
+		pbChooseTradablePokemon(36, 37,
+			proc { |pkmn| pkmn.isSpecies?(:CRUSTANG) }
+		)
+	end
 end
+
--- Plugins/Crustang Racing/Settings.rb
+++ Plugins/Crustang Racing/Settings.rb
@@ -80,4 +80,14 @@ CONTESTANTS = [
 {TrainerName: "Luis", TrainerCharacter: "NPC 02", PkmnName: "Santa Claws", Moves: [:ROCKTOMB, :RAPIDSPIN, :HELPINGHAND]}, #gives: rockHazard, secondBoost, overload
 {TrainerName: "Trevor", TrainerCharacter: "NPC 03", PkmnName: "Crusty", Moves: [:VISEGRIP, :REST, :MUDSLAP]}, #gives: spinOut, reduceCooldown, mudHazard
 ]
+
+#========================================================#
+#=============== RENTAL CRUSTANG SETTINGS ===============#
+#========================================================#
+CONTESTANTS = [
+{TrainerName: "Rental Ron", PkmnName: "Miss Jeavious", Moves: [:VISEGRIP, :IRONDEFENSE, :HELPINGHAND]}, #gives: spinOut, invincible, overload
+{TrainerName: "Rental Ron", PkmnName: "Mister Crab", Moves: [:ROCKTOMB, :RAPIDSPIN, :HELPINGHAND]}, #gives: rockHazard, secondBoost, overload
+{TrainerName: "Rental Ron", PkmnName: "Striker", Moves: [:VISEGRIP, :REST, :MUDSLAP]}, #gives: spinOut, reduceCooldown, mudHazard
+]
+
 end #class CrustangRacingSettings
