1208e15253d95325b6fffb9539e7934e1eb18273
LowEffortCoding <nathanbp2000@gmail.com>
2024-09-23 21:15:33 -0300

    exp fixes

--- Plugins/Multi Exp Panel/Battle.rb
+++ Plugins/Multi Exp Panel/Battle.rb
@@ -10,23 +10,23 @@ class Battle
     expAll = true
     p1 = pbParty(0)
     @battlers.each do |b|
-      next unless b&.opposes?   # Can only gain Exp from fainted foes
-      next if b.participants.length == 0
-      next unless b.fainted? || b.captured
-      # Count the number of participants
-      numPartic = 0
+		next unless b&.opposes?   # Can only gain Exp from fainted foes
+		next if b.participants.length == 0
+		next unless b.fainted? || b.captured
+		# Count the number of participants
+		numPartic = 0
 		if $game_variables[MECHANICSVAR] >= 2
 			eachInTeam(0, 0) do |pkmn, i|
 				b.participants.push(i) # brute forcing my way to get this thing to have all possible allys to be "participants"
 			end
 			b.participants.uniq! # removing repeats
 		end
-      b.participants.each do |partic|
-        next unless pbIsOwner?(0, partic)
-        numPartic += 1
-      end
-      # Find which Pokémon have an Exp Share
-      expShare = [] # filler
+		b.participants.each do |partic|
+			next unless pbIsOwner?(0, partic)
+			numPartic += 1
+		end
+      	# Find which Pokémon have an Exp Share
+      	expShare = [] # filler
 		haveexpleech = 0
 		expleechtargets = []
 		eachInTeam(0, 0) do |pkmn, i|
@@ -38,46 +38,46 @@ class Battle
 		vanillaStuff = true if $bag.has?(:EXPALLOFF)
 		expAll = false if haveexpleech>0
 		expAll = false if vanillaStuff
-      # Calculate EV and Exp gains for the participants
-			if expAll
-				# Gain Exp for all Pokémon due to no Exp Leech users
+     	# Calculate EV and Exp gains for the participants
+		if expAll
+			# Gain Exp for all Pokémon due to no Exp Leech users
+			eachInTeam(0, 0) do |pkmn, i|
+				values[i] = pbGainExpOne_Panel(i, b, numPartic, expShare, expAll, true)
+			end
+		else
+			if !vanillaStuff
+				# Gain Exp for Exp Leech users
 				eachInTeam(0, 0) do |pkmn, i|
-					values[i] = pbGainExpOne_Panel(i, b, numPartic, expShare, expAll, true)
+					next unless expleechtargets.include?(i)
+					values[i] = pbGainExpOne_Panel(i, b, haveexpleech, expleechtargets, expAll, !pkmn.shadowPokemon?)
 				end
 			else
-				if !vanillaStuff
-					# Gain Exp for Exp Leech users
-					eachInTeam(0, 0) do |pkmn, i|
-						next unless expleechtargets.include?(i)
-						values[i] = pbGainExpOne_Panel(i, b, haveexpleech, expleechtargets, expAll, !pkmn.shadowPokemon?)
+				eachInTeam(0, 0) do |pkmn, i|
+					unless b.participants.include?(i)
+						values[i] = 0
+						next
 					end
-				else
-					eachInTeam(0, 0) do |pkmn, i|
-						unless b.participants.include?(i)
-							values[i] = 0
-							next
-						end
-						values[i] = pbGainExpOne_Panel(i, b, numPartic, expShare, expAll, true)
-					end
+					values[i] = pbGainExpOne_Panel(i, b, numPartic, expShare, expAll, true)
 				end
 			end
-      vr = []
-			totalexps=0
-      for v in values
-        t_v = v ? v : 0
-        vr.push(t_v)
+		end
+		vr = []
+		totalexps=0
+		for v in values
+			t_v = v ? v : 0
+			vr.push(t_v)
 			totalexps+=v.to_i
-      end
-			if totalexps>0
-				values = vr
-				s = Swdfm_Exp_Screen.new(values)
-				# Clear the participants array
-				for i in 0...$player.party.size
-					next if values[i] == 0
-					pbActualLevelUpAndGatherMoves(i, values[i])
-				end
+		end
+		if totalexps>0
+			values = vr
+			s = Swdfm_Exp_Screen.new(values)
+			# Clear the participants array
+			for i in 0...$player.party.size
+				next if values[i] == 0
+				pbActualLevelUpAndGatherMoves(i, values[i])
 			end
-      b.participants = []
+		end
+     	b.participants = []
     end
   end
   
@@ -107,26 +107,27 @@ class Battle
     return 0 if exp <= 0
 	# level cap #by low
 	expvariable = ($game_switches[LOWEREXPGAINSWITCH]) ? 50 : 33
-	truelevel = defeatedBattler.level								# stuff
-	truelevel -= 10 if $game_variables[MASTERMODEVARS][7]==true		# for
-	truelevel -= 20 if $game_variables[MASTERMODEVARS][22]==true	# master
-	truelevel -= 30 if $game_variables[MASTERMODEVARS][24]==true	# mode
-	truelevel -= 60 if $game_variables[MASTERMODEVARS][27]==true	# settings
-	if (pkmn.level - truelevel) >= 3 && $game_variables[MECHANICSVAR] == 0
-		truelevel = (pkmn.level - 5)
-		expvariable = 5
-	end
+	truelevel = defeatedBattler.level
+	truelevel = (defeatedBattler.level - 5) if (pkmn.level - defeatedBattler.level) >= 3 && 
+									 	 	 	$game_variables[MECHANICSVAR] == 0
+	truelevel -= 10 if $game_variables[MASTERMODEVARS][7]==true
+	truelevel -= 20 if $game_variables[MASTERMODEVARS][22]==true
+	truelevel -= 30 if $game_variables[MASTERMODEVARS][24]==true
+	truelevel -= 60 if $game_variables[MASTERMODEVARS][27]==true
 	exp = (exp / 3).floor
+	originExp = exp if $game_variables[MECHANICSVAR] == 0
 	exp = (exp * (100 + expvariable * (truelevel - pkmn.level)) / 100).floor
-	exp = 0 if pkmn.level - truelevel == 3
+	exp = 0 if pkmn.level - truelevel >= 3 && $game_variables[MECHANICSVAR] > 0
+	exp = (originExp * 0.1).floor if exp <= 0 && 
+									 $game_variables[MECHANICSVAR] == 0 && 
+									!$game_switches[LOWEREXPGAINSWITCH]
 	exp = (exp / 2).floor if pkmn.level>40
 	#exp = (exp * 0.2).floor if $game_switches[319] 				# custom wild
 	#exp = 0 if $game_switches[305] && pkmn.level>=level 		# leader rematch
 	# exp leech #by low
 	if !expAll
-		# exp is multiplied by (number of allies in party) / (number of allies with exp leech) 
-		# 100.0 so we get some not round numbers
-		exp *= ((defeatedBattler.participants.length)*100)/(numPartic*100.0)
+		# exp is multiplied by (number of allies in party) / (number of allies with exp leech)
+		exp *= ((defeatedBattler.participants.length)*100)/(numPartic*100.0).floor
 	end
 	pbGainEVsDemICE(idxParty, defeatedBattler, numPartic, expShare, expAll, showMessages)
     # Make sure Exp doesn't exceed the maximum
