4e8f899c62a47a2dbe5472e9b5245afb7fbfe5f6
Gardenette <106409863+Gardenette@users.noreply.github.com>
2024-11-01 20:24:16 -0400

    Autoscroll plugin - autoscroll is fixed!

--- Plugins/Autoscroll/Main.rb
+++ Plugins/Autoscroll/Main.rb
@@ -81,55 +81,63 @@ class Interpreter
   #     speed : (optional) scroll speed (from 1-6, default being 4)
   #-----------------------------------------------------------------------------
   def autoscroll(x, y, speed = SCROLL_SPEED_DEFAULT)
-    if $game_map.scrolling?
-      return false
-    elsif !$game_map.valid?(x, y)
-      print "Map Autoscroll: given x,y is invalid"
-      return command_skip
-    elsif !(1..6).include?(speed)
-      print "Map Autoscroll: invalid speed (1-6 only)"
-      return command_skip
-    end
-    center_x = ((Graphics.width / 2) - (Game_Map::TILE_WIDTH / 2)) * 4   # X coordinate in the center of the screen
-    center_y = ((Graphics.height / 2) - (Game_Map::TILE_HEIGHT / 2)) * 4   # Y coordinate in the center of the screen
-    max_x = ($game_map.width - (Graphics.width.to_f / Game_Map::TILE_WIDTH)) * 4 * Game_Map::TILE_WIDTH
-    max_y = ($game_map.height - (Graphics.height.to_f / Game_Map::TILE_HEIGHT)) * 4 * Game_Map::TILE_HEIGHT
-    count_x = ($game_map.display_x - [0, [(x * Game_Map::REAL_RES_X) - center_x, max_x].min].max) / Game_Map::REAL_RES_X
-    count_y = ($game_map.display_y - [0, [(y * Game_Map::REAL_RES_Y) - center_y, max_y].min].max) / Game_Map::REAL_RES_Y
-    if @diag
-      @diag = false
-      dir = nil
-      if count_x != 0 && count_y != 0
-        return false
-      elsif count_x > 0
-        dir = 4
-      elsif count_x < 0
-        dir = 6
-      elsif count_y > 0
-        dir = 8
-      elsif count_y < 0
-        dir = 2
-      end
-      count = count_x == 0 ? count_y.abs : count_x.abs
-    else
-      @diag = true
-      dir = nil
-      if count_x > 0
-        if count_y > 0
-          dir = 7
-        elsif count_y < 0
-          dir = 1
-        end
-      elsif count_x < 0
-        if count_y > 0
-          dir = 9
-        elsif count_y < 0
-          dir = 3
-        end
-      end
-      count = [count_x.abs, count_y.abs].min
-    end
-    $game_map.start_scroll(dir, count, speed) if dir
+	2.times do
+		if $game_map.scrolling?
+			return false
+		elsif !$game_map.valid?(x, y)
+		print "Map Autoscroll: given x,y is invalid"
+		return command_skip
+		elsif !(1..6).include?(speed)
+		print "Map Autoscroll: invalid speed (1-6 only)"
+		return command_skip
+		end
+		center_x = ((Graphics.width / 2) - (Game_Map::TILE_WIDTH / 2)) * 4   # X coordinate in the center of the screen
+		center_y = ((Graphics.height / 2) - (Game_Map::TILE_HEIGHT / 2)) * 4   # Y coordinate in the center of the screen
+		max_x = ($game_map.width - (Graphics.width.to_f / Game_Map::TILE_WIDTH)) * 4 * Game_Map::TILE_WIDTH
+		max_y = ($game_map.height - (Graphics.height.to_f / Game_Map::TILE_HEIGHT)) * 4 * Game_Map::TILE_HEIGHT
+		count_x = ($game_map.display_x - [0, [(x * Game_Map::REAL_RES_X) - center_x, max_x].min].max) / Game_Map::REAL_RES_X
+		count_y = ($game_map.display_y - [0, [(y * Game_Map::REAL_RES_Y) - center_y, max_y].min].max) / Game_Map::REAL_RES_Y
+		if @diag
+		@diag = false
+		dir = nil
+		if count_x != 0 && count_y != 0
+			return false
+		elsif count_x > 0
+			dir = 4
+		elsif count_x < 0
+			dir = 6
+		elsif count_y > 0
+			dir = 8
+		elsif count_y < 0
+			dir = 2
+		end
+		count = count_x == 0 ? count_y.abs : count_x.abs
+		else
+		@diag = true
+		dir = nil
+		if count_x > 0
+			if count_y > 0
+			dir = 7
+			elsif count_y < 0
+			dir = 1
+			end
+		elsif count_x < 0
+			if count_y > 0
+			dir = 9
+			elsif count_y < 0
+			dir = 3
+			end
+		end
+		count = [count_x.abs, count_y.abs].min
+		end
+		$game_map.start_scroll(dir, count, speed) if dir
+		#player will wait while scrolling
+		while $game_map.scrolling?
+			Graphics.update
+			$game_map.update
+			pbUpdateSceneMap
+		end
+	end #2.times do
     return !@diag
   end
 
