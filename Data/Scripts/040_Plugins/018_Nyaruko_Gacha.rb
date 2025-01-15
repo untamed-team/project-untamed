4f75eaac94f00d51102739d5acb1d0aea5d24dc2
LowEffortCoding <105573664+LowEffortCoding@users.noreply.github.com>
2025-01-15 15:58:00 -0300

    Update 018_Nyaruko_Gacha.rb

--- Data/Scripts/040_Plugins/018_Nyaruko_Gacha.rb
+++ Data/Scripts/040_Plugins/018_Nyaruko_Gacha.rb
@@ -64,10 +64,6 @@ def gachaPullsNPC
       if selectedCommander == "Pull #{pullamount} time(s) at the cost of #{totalpullcost} coins?"
         pbPlayer.coins -= totalpullcost
         LootBox.new.pbStartMainScene(pullamount)
-        if $game_variables[GACHA_USED] % 100 == 0
-          pbMessage(_INTL("Congratulations Highroller! You've earned a Gold Milage Ticket for your dedication!"))
-          $PokemonGlobal.ticketStorage.push("Gold Milage Ticket")
-        end
         pbMessage(_INTL("Thank you, Highroller. Please come again!"))
         return true
       end
@@ -185,6 +181,11 @@ class LootBox
               item = semiRandomRNG(common.length, gachaamt)
               sprites["icon#{i}"].item = GameData::Item.get(common[item]).id
               pbReceiveItem(common[item])
+            end
+            
+            if $game_variables[GACHA_USED] % 100 == 0
+              pbMessage(_INTL("Congratulations Highroller! You've earned a Gold Milage Ticket for your dedication!"))
+              $PokemonGlobal.ticketStorage.push("Gold Milage Ticket")
             end
         end
         pbWait(10)
