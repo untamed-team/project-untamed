b02954e503875ef7b00ce03d1dba67dc5e2eea98
Gardenette <106409863+Gardenette@users.noreply.github.com>
2025-02-21 20:24:02 -0500

    Gard Utils - remove partner npc when blacking out

--- Data/Scripts/044_Untamed Custom Misc/004_Gardenette's Utilities.rb
+++ Data/Scripts/044_Untamed Custom Misc/004_Gardenette's Utilities.rb
@@ -2112,3 +2112,63 @@ def pbPlayerReceiveMoney(amount, multiplier=1)
   pbMessage("\\PN received $#{amount}!")
   $player.money += amount
 end #def pbPlayerReceiveMoney
+
+#==================================================================================
+# Blacking out animation - edited to deregister partner trainer upon blacking out
+#==================================================================================
+def pbStartOver(gameover = false)
+  if pbInBugContest?
+    pbBugContestStartOver
+    return
+  end
+  $stats.blacked_out_count += 1
+  $player.heal_party
+  if $PokemonGlobal.pokecenterMapId && $PokemonGlobal.pokecenterMapId >= 0
+    if gameover
+      pbMessage(_INTL("\\w[]\\wm\\c[8]\\l[3]After the unfortunate defeat, you scurry back to a Pokémon Center."))
+    else
+      pbMessage(_INTL("\\w[]\\wm\\c[8]\\l[3]You scurry back to a Pokémon Center, protecting your exhausted Pokémon from any further harm..."))
+    end
+    pbCancelVehicles
+    Followers.clear
+    pbDeregisterPartner #added by Gardenette
+    $game_switches[Settings::STARTING_OVER_SWITCH] = true
+    $game_temp.player_new_map_id    = $PokemonGlobal.pokecenterMapId
+    $game_temp.player_new_x         = $PokemonGlobal.pokecenterX
+    $game_temp.player_new_y         = $PokemonGlobal.pokecenterY
+    $game_temp.player_new_direction = $PokemonGlobal.pokecenterDirection
+    $scene.transfer_player if $scene.is_a?(Scene_Map)
+    $game_map.refresh
+  else
+    homedata = GameData::PlayerMetadata.get($player.character_ID)&.home
+    homedata = GameData::Metadata.get.home if !homedata
+    if homedata && !pbRgssExists?(sprintf("Data/Map%03d.rxdata", homedata[0]))
+      if $DEBUG
+        pbMessage(_ISPRINTF("Can't find the map 'Map{1:03d}' in the Data folder. The game will resume at the player's position.", homedata[0]))
+      end
+      $player.heal_party
+      return
+    end
+    if gameover
+      pbMessage(_INTL("\\w[]\\wm\\c[8]\\l[3]After the unfortunate defeat, you scurry back home."))
+    else
+      pbMessage(_INTL("\\w[]\\wm\\c[8]\\l[3]You scurry back home, protecting your exhausted Pokémon from any further harm..."))
+    end
+    if homedata
+      pbCancelVehicles
+      Followers.clear
+      pbDeregisterPartner #added by Gardenette
+      $game_switches[Settings::STARTING_OVER_SWITCH] = true
+      $game_temp.player_new_map_id    = homedata[0]
+      $game_temp.player_new_x         = homedata[1]
+      $game_temp.player_new_y         = homedata[2]
+      $game_temp.player_new_direction = homedata[3]
+      $scene.transfer_player if $scene.is_a?(Scene_Map)
+      $game_map.refresh
+    else
+      $player.heal_party
+      pbDeregisterPartner #added by Gardenette
+    end
+  end
+  pbEraseEscapePoint
+end
