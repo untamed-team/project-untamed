8cfcf7c46dabf22511b55697e0df8a9b4871f745
Gardenette <106409863+Gardenette@users.noreply.github.com>
2025-03-28 20:47:06 -0400

    fixed issue with custom credits script
    It's a plugin now too

---
+++ Plugins/Custom Credits/Credit Names.rb
@@ -0,0 +1,26 @@
+class Scene_Credits
+  #hash that holds the names in credits that have graphics to display
+  #this has also holds the name of the graphics
+  #syntax:
+  #{ "Name in credits" => "file name" }
+  NAMES_HASH = {
+  "Akikaze" => "Akikaze",
+  "Bean Soup" => "Bean Soup",
+  "BlueLightning" => "BlueLightning",
+  "Chespin_Craft" => "Chespin_Craft",
+  "Gardenette" => "Gardenette",
+  "GreenJellyBean" => "GreenJellyBean",
+  "Jeremy K" => "Jeremy K",
+  "Kaloja" => "Kaloja",
+  "Kutas Kitti" => "Kutas Kitti",
+  "Low" => "Low",
+  "MadMillerMiller" => "MadMillerMiller",
+  "MasterMachi" => "MasterMachi",
+  "Saltilton" => "Saltilton",
+  "SpaceWestern" => "SpaceWestern",
+  "Spork" => "Spork",
+  "stygma" => "stygma",
+  "Tyler Jones" => "Tyler Jones",
+  "VentesTheFloof" => "VentesTheFloof",
+  }
+end #class Scene_Credits
