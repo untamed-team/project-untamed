c48e8bbed2a02cb6dd1e855b8c887a5d31683048
Delphine <80095054+delphine-birch@users.noreply.github.com>
2025-03-31 19:37:58 +0100

    Masuda method also accounted for

--- Plugins/Full Odds Tracker/Script.rb
+++ Plugins/Full Odds Tracker/Script.rb
@@ -1,3 +1,14 @@
+#==============================================================================
+# Full Odds Tracker
+# This adds the method 'full_odds' to pokemon, that returns false if any shiny
+# odds affecting mechanics are in play. It does it's best to catch this at
+# all passes - if the shininess is re-rolled, if the shininess is set to true,
+# or if specific mechanics are in play like the shiny charm.
+#
+# This should catch mostly everything, but you can edit the event handlers to
+# add some custom stuff if you want.
+#==============================================================================
+
 class Pokemon
   # Number of times this pokemon has rolled for shininess
   # Can be set to 2 to manually mark as not-full-odds
@@ -15,7 +26,7 @@ class Pokemon
     if @shiny.nil?
       @shiny_roll_count = 0 if @shiny_roll_count.nil?
       @shiny_roll_count = @shiny_roll_count + 1
-      echoln "Rolling for shininess..."
+      # echoln "Rolling for shininess..."
     end
     return fo_shiny?
   end
@@ -25,21 +36,38 @@ class Pokemon
   def shiny=(value)
     @shiny_roll_count = 2 if value == true
     if value == true
-        echoln "Invalidated due to set shiny"
+        # echoln "Invalidated due to set shiny"
     end
     @shiny = value
   end
 end
 
+# Accounting for Masuda method for increasing shiny chance
+class DayCare
+  module EggGenerator
+    alias set_shininess fo_set_shininess
+    def set_shininess(egg, mother, father)
+      if father.owner.language != mother.owner.language
+        egg.shiny_roll_count = 2;
+      end
+      fo_set_shininess(egg, mother, father)
+    end
+  end
+end
+      
+
+# Manually marks as not-full-odds for scenarios where shiny odds are effected - 
+# redundant except in very rare situations (rolled a shiny on the first try 
+# despite having multiple tries due to some shiny_chance affecting mechanic)
 EventHandlers.add(:on_wild_pokemon_created, :full_odds_switch,
   proc { |pkmn|
     if $bag.has?(:SHINYCHARM)
         pkmn.shiny_roll_count = 2
-        echoln "Invalidated due to shiny charm"
+        # echoln "Invalidated due to shiny charm"
     end
     if $player.pokedex.battled_count(pkmn.species) > 51 && Settings::HIGHER_SHINY_CHANCES_WITH_NUMBER_BATTLED
         pkmn.shiny_roll_count = 2
-        echoln "Invalidated due to higher shiny chance from number fought"
+        # echoln "Invalidated due to higher shiny chance from number fought"
     end
   }
 )
