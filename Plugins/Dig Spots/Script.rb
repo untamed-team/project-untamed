ef12acbd268c97a500a7effeda00ed5b7d2cbe7e
Gardenette <106409863+Gardenette@users.noreply.github.com>
2025-05-28 20:31:49 -0400

    working on "gold panning"

---
+++ Plugins/Dig Spots/Script.rb
@@ -0,0 +1,21 @@
+class DigSpots
+  def initialize
+	#go through all events on the current map and only do something if an event on the map is named "DigSpot"
+	$game_map.events.each_value do |event|
+      next if event.name != "DigSpot"
+      print "found a DigSpot"
+    end
+  end #def generateDigSpotsOnMap
+  
+end #class DigSpots
+
+EventHandlers.add(:on_enter_map, :spawn_dig_spots,
+  proc { |_old_map_id|
+	oldMapID = _old_map_id
+	currentMapID = $game_map.map_id
+	
+	#if oldMapID and currentMapID match, the game was just loaded. Skip generating dig spots
+	next if oldMapID == currentMapID
+	#DigSpots.new
+  }
+)
