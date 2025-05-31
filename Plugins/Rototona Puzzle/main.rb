a92266bc30ed0d62c328698947332124a4278e71
Gardenette <106409863+Gardenette@users.noreply.github.com>
2025-05-30 21:59:35 -0400

    Working on rotopuzzle

--- Plugins/Rototona Puzzle/main.rb
+++ Plugins/Rototona Puzzle/main.rb
@@ -1,40 +1,56 @@
 #=============================================================================
 # Rototona Puzzle
 #=============================================================================
-def self.initialize
-	#when entering the map with the puzzle
-	self.getPuzzleEvents
-	#self.resetRototonas #might only be necessary for debug purposes
-end #self.initialize
+class RototonaPuzzle
+	def initialize
+		#when entering the map with the puzzle
+		getPuzzleEvents
+		#self.resetRototonas #might only be necessary for debug purposes
+	end #self.initialize
 
-def self.getPuzzleEvents
-	#identify all the events on the map which correspond with the puzzle
-	puzzleEvents = {
-		:Rototona1      => nil,
-		:Rototona2      => nil,
-		:Launcher1      => nil,
-		:Launcher2      => nil,
-		:Catcher1       => nil,
-		:Catcher2       => nil,
-		:Barriers       => [],
-		:Ramps          => [],
-		:StraightTracks => [],
-		:CornerTracks   => []
-	}
-	$game_map.events.each_value do |event|
-      puzzleEvents[:Rototona1] = event if event.name == "Rototona1"
-	  puzzleEvents[:Rototona2] = event if event.name == "Rototona2"
-	  puzzleEvents[:Launcher1] = event if event.name == "Launcher1"
-	  puzzleEvents[:Launcher2] = event if event.name == "Launcher2"
-	  puzzleEvents[:Catcher1] = event if event.name == "Catcher1"
-	  puzzleEvents[:Catcher2] = event if event.name == "Catcher2"
-	  puzzleEvents[:Barriers].push(event) if event.name == "Barrier"
-	  puzzleEvents[:Ramps].push(event) if event.name == "Ramp"
-	  puzzleEvents[:StraightTracks].push(event) if event.name == "StraightTrack"
-	  puzzleEvents[:CornerTracks].push(event) if event.name == "CornerTrack"
-    end
-end #def self.getPuzzleEvents
+	def getPuzzleEvents
+		#identify all the events on the map which correspond with the puzzle
+		print "identifying puzzle pieces on the map"
+		puzzleEvents = {
+			:Rototona1      => nil,
+			:Rototona2      => nil,
+			:Launcher1      => nil,
+			:Launcher2      => nil,
+			:Catcher1       => nil,
+			:Catcher2       => nil,
+			:Barriers       => [],
+			:Ramps          => [],
+			:StraightTracks => [],
+			:CornerTracks   => []
+		}
+		$game_map.events.each_value do |event|
+			puzzleEvents[:Rototona1] = event if event.name == "RotoPuzzle_Rototona1.shadowless"
+			puzzleEvents[:Rototona2] = event if event.name == "RotoPuzzle_Rototona2.shadowless"
+			puzzleEvents[:Launcher1] = event if event.name == "RotoPuzzle_Launcher1.shadowless"
+			puzzleEvents[:Launcher2] = event if event.name == "RotoPuzzle_Launcher2.shadowless"
+			puzzleEvents[:Catcher1] = event if event.name == "RotoPuzzle_Catcher1.shadowless"
+			puzzleEvents[:Catcher2] = event if event.name == "RotoPuzzle_Catcher2.shadowless"
+			puzzleEvents[:Barriers].push(event) if event.name == "RotoPuzzle_Barrier.shadowless"
+			puzzleEvents[:Ramps].push(event) if event.name == "RotoPuzzle_Ramp.shadowless"
+			puzzleEvents[:StraightTracks].push(event) if event.name == "RotoPuzzle_StraightTrack.shadowless"
+			puzzleEvents[:CornerTracks].push(event) if event.name == "RotoPuzzle_CornerTrack.shadowless"
+		end
+	end #def self.getPuzzleEvents
 
-def self.resetRototonas
-	
-end #def self.resetRototonas
+	def interact
+		print "interacting"
+		print "Barrier events are #{puzzleEvents[:Barriers]}"
+	end #def self.interact
+
+	def resetRototonas
+		
+	end #def self.resetRototonas
+end #class RototonaPuzzle
+
+#on_player_interact with puzzle event
+EventHandlers.add(:on_player_interact, :interact_with_puzzle_event, proc {
+	#skip this check if not on Canyon Temple Left and Canyon Temple Right maps
+	next if $game_map.map_id != 59 && $game_map.map_id != 120
+	facingEvent = $game_player.pbFacingEvent
+	RototonaPuzzle.interact if facingEvent && facingEvent.name.match(/RotoPuzzle/i)
+})
