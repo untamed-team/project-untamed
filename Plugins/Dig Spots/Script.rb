32b7ea238b580394049ebf99e11ea99b94381cd1
Gardenette <gardenette1995@gmail.com>
2025-08-08 04:57:41 -0400

    Deleted old rotatona puzzle variables from $game_varibales

--- Plugins/Dig Spots/Script.rb
+++ Plugins/Dig Spots/Script.rb
@@ -1,3 +1,8 @@
+#summary
+#
+
+#To do
+
 class DigSpots
   def initialize
 	#go through all events on the current map and only do something if an event on the map is named "DigSpot"
@@ -11,11 +16,9 @@ end #class DigSpots
 
 EventHandlers.add(:on_enter_map, :spawn_dig_spots,
   proc { |_old_map_id|
-	oldMapID = _old_map_id
-	currentMapID = $game_map.map_id
-	
+
 	#if oldMapID and currentMapID match, the game was just loaded. Skip generating dig spots
-	next if oldMapID == currentMapID
-	#DigSpots.new
+	next if _old_map_id == $game_map.map_id
+	DigSpots.new
   }
 )
