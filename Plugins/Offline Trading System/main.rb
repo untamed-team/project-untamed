9091b00a840e9f3dcb3214681e75bb6b214d8f7e
Gardenette <gardenette1995@gmail.com>
2025-08-30 14:53:28 -0400

    OTS: got folder to open. Next is reading image in folder to make agreement

--- Plugins/Offline Trading System/main.rb
+++ Plugins/Offline Trading System/main.rb
@@ -5,6 +5,8 @@ class Game_Player < Game_Character
 end
 
 class OfflineTradingSystem
+	TRADE_FILE_PATH = "Trading/Trade.png"
+	
 	ELIGIBLE_CHARACTERS = ["A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h","I","i","J","j","K","k","L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s","T","t","U","u","V","v","W","w","X","x","Y","y","Z","z","1","2","3","4","5","6","7","8","9","0"]
 	
 	ENCODER_MAPPING = {
@@ -103,8 +105,10 @@ class OfflineTradingSystem
 		#save pokemon symbol as it will be used to delete the exact pokemon later
 		$game_variables[1] = pkmn
 		createOfferImage(pkmn)
+		
 		#the player then gives the offer to another player, who gives the original player THEIR offer image
 		#the player replaces Trade.png in the "Trading" folder with the offer image from the other player, and then the original player proceeds in game
+		self.readOfferImage(TRADE_FILE_PATH)
 		##########################self.promptAcceptTrade
 		
 		#the game then asks the player if they wish to accept this trade (showing them the pkmn they would get and giving them the option to look at the summary screen)
@@ -122,7 +126,7 @@ class OfflineTradingSystem
 		#this method takes the marshaldata of a pkmn offered for trading and turns the marshaldata into a hexadecimal format
 		#a string is created (encoded_hex_data), which is "playerTradeID_pkmnInHex" but encoded (3x as long)
 		#that string is then added to an image of the pkmn, which is created in the "Trading" folder. The image is named "Trade.png"
-		msgwindow = pbMessageNoClear(_INTL("\\wtnp[1]Generating offer..."))
+		pbMessage(_INTL("\\wtnp[1]Generating offer..."))
 		playerTradeID = $game_player.tradeID
 		pokemon_to_save = pkmn
 		serialized_data = Marshal.dump(pokemon_to_save)
@@ -146,18 +150,37 @@ class OfflineTradingSystem
 		
 		#hide hex data in image metadata
 		# Make sure to define your hex data and file path first
-		file_path = "Trading/Trade.png" # Assuming this is the correct path to your image
 		# 1. Encode the data
-		success = add_text_to_png(file_path, encoded_hex_data)
+		success = add_text_to_png(TRADE_FILE_PATH, encoded_hex_data)
 
 		if success
 			puts "Adding text to png successful! The image should now contain the encoded hex data."
-			self.readOfferImage(file_path)
 		else
 			puts "Adding text to png failed."
 			print "do something to try again"
 		end
-		msgwindow.clear
+		
+		command_list = [_INTL("Open Trade Folder"),_INTL("Check Offer"),_INTL("Cancel Trade")]
+		# Main loop
+		command = 0
+		ready = false
+		loop do
+			choice = pbMessage(_INTL("Give Trade.png to the person you're trading with. Replace your Trade.png with their Trade.png."), command_list, -1, nil, command)
+			case choice
+			when 0
+				root_folder = RTP.getPath('.', "Game.ini")
+				system("start explorer \"#{root_folder}\\Trading\"")
+			when 1
+				ready = true
+			when 2
+				break
+			end #case choice
+		end #loop do
+		if ready
+			print "player chose to move on, so now we read the code again and combine 1st code and second code to create agreement"
+		else
+			#do nothing so we go back to PC box
+		end
 	end #def self.createOfferImage
 
 	def self.encode(data)
@@ -193,9 +216,9 @@ class OfflineTradingSystem
 		bitmap.to_file("Trading/Trade.png")
 	end #def self.saveTradeOfferBitmap
 
-	def self.readOfferImage(file_path)
+	def self.readOfferImage(trade_file_path)
 		# 2. Decode the data and capture the return value
-		text_from_png = get_text_from_png(file_path)
+		text_from_png = get_text_from_png(trade_file_path)
 		if !text_from_png
 			puts "Getting text from png failed."
 			print "do something to try again"
@@ -203,7 +226,12 @@ class OfflineTradingSystem
 		
 		puts "Getting text from png successful!"
 		puts "Encoded hex from png: #{text_from_png}"
-		
+		###############check the quests plugin (in IDE) to see how I split using a delimiter
+		arrayOfText = text_from_png.split("_")
+		@otherPlayerTradeID = arrayOfText[0]
+		@pkmnOtherPlayerIsOfferingEncoded = arrayOfText[1]
+		print "other player's tradeID is #{@otherPlayerTradeID}"
+		Console.echo_warn "@pkmnOtherPlayerIsOfferingEncoded is #{@pkmnOtherPlayerIsOfferingEncoded}"
 		#the game then extracts the text from that offer image, obtaining the encoded hex and other player's trade ID
 		#the tradeID is extracted into its own variable - @otherPlayerTradeID
 		#everything up to the _ in the encoded hex is deleted, and the _ is deleted too, so all that remains is the pkmn
