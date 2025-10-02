a1e59200a786c686cd5cd3880b254c4fd224e790
ExiasNoob <nathanbp2000@gmail.com>
2025-10-02 11:17:03 -0300

    hidden power fix

--- Data/Scripts/036_Enhanced UI/003_Battle UI.rb
+++ Data/Scripts/036_Enhanced UI/003_Battle UI.rb
@@ -913,7 +913,9 @@ class Battle::Scene
     cord = 0
     battler.eachMove do |m|
       next if !@battle.moveRevealed?(battler, m.id) && !battler.pbOwnedByPlayer?
-      addText.push([m.name, xpos + 103, ypos + 140 + (cord * 24), 0, BASE_LIGHT, SHADOW_LIGHT])
+      move_name = m.name
+      move_name = "HidPwr #{GameData::Type.get(battler.hptype).name}" if m.function == "TypeDependsOnUserIVs"
+      addText.push([move_name, xpos + 103, ypos + 140 + (cord * 24), 0, BASE_LIGHT, SHADOW_LIGHT])
       cord += 1
     end
     return images, addText
